a7a643a435b3a99eb76ddff84220c9aeb132bd64
s.arabi <s.arabi@samin.local>
2022-03-15 03:12:29 -0700

    first commit

---
+++ Factory/Program.cs
@@ -0,0 +1,20 @@
+﻿using FactoryClass;
+using Interface;
+using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intCustomerType = 0;
+            ICustomer objCust;
+            Console.WriteLine("Enter the customer type(1 0r 2)");
+            intCustomerType = Convert.ToInt16(Console.ReadLine());
+            objCust = clsFactory.getCustomerType(intCustomerType);
+            objCust.Report();
+            Console.ReadLine();
+        }
+    }
+}
